c533bb1dfc6e7744b640b2c9ebcef70b31f7b7a1
Alexandra <Polyakova.Al.Vi@sberbank.ru>
2019-08-30 20:56:29 +0300

    raect app

--- src/Deposit.js
+++ src/Deposit.js
@@ -5,6 +5,12 @@ import "./Deposit.css";
 
 export default function Deposit(props) {
     let result = 100000;
+    var formatter = new Intl.NumberFormat("ru", {
+        style: "currency",
+        currency: "RUB",
+        minimumFractionDigits: 0
+    });
+
     return (
             <div className={'wrap'}>
                 <div className={'wrap-img'}><img src={props.img} /></div>
@@ -14,19 +20,25 @@ export default function Deposit(props) {
                     <div className={'main-info'}>
                         <div className={'result sum'}>
                             <div>Вы получите</div>
-                            <div>{(result*(props.rate/100)) + result}</div>
+                            <div>{formatter.format((result*(props.rate/100)) + result)}</div>
                         </div>
                         <div className={'result rate'}>
                             <div>Ставка</div>
                             <div>{`${props.rate} %`}</div>
                         </div>
                         <div className={'result add-remove'}>
-                            <div>{props.removing ? 'С частичным снятием' : 'Без частичного снятия'}</div>
-                            <div>{props.adding ? 'С пополнением' : 'Без пополнения'}</div>
+                            <div>{props.removing ?
+                                <div className={'check'}>С частичным снятием</div> :
+                                <div className={'denial'}>Без частичного снятия</div>}
+                            </div>
+                            <div>{props.adding ?
+                                <div className={'check'}>С пополнением</div> :
+                                <div className={'denial'}>Без пополнения</div>}
+                            </div>
                         </div>
                     </div>
-                    <button className={'open-button'}>Открыть вклад</button>
-                    <button className={'info-button'}>Подробнее</button>
+                    <button className={'open button'}>Открыть вклад</button>
+                    <button className={'info button'}>Подробнее</button>
                 </div>
             </div>
     );
